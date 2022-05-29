b61a4b529c5bc6ebbe7708213b23cefa738be05c
wrench <me@jyothis.gq>
2022-05-29 22:01:54 +0530

    EXPERIMENTAL: add caching for home route

--- app/api/routes/home.py
+++ app/api/routes/home.py
@@ -3,7 +3,7 @@ import random
 from copy import deepcopy
 from fastapi import APIRouter
 from typing import Dict, Union
-
+from diskcache import Cache
 
 router = APIRouter(
     # dependencies=[Depends(get_token_header)],
@@ -18,86 +18,103 @@ data_cap_limit = 15
 @router.get("", response_model=Dict[str, Union[str, int, float, bool, None, dict]])
 def home() -> Dict[str, str]:
     start = time.perf_counter()
-    from main import metadata
+    with Cache("cache", timeout=0) as cache:
+        data = cache.get("data") or {}
+        if not data:
+            from main import metadata
 
-    random.seed(100)
-    categories_data = []
-    carousel_data = []
-    top_rated_series_data = []
-    top_rated_movies_data = []
-    newly_added_movies_data = []
-    newly_added_episodes_data = []
-    unwanted_keys = [
-        "cast",
-        "seasons",
-        "file_name",
-        "subtitles",
-        "external_ids",
-        "collection",
-        "homepage",
-        "last_episode_to_air",
-        "next_episode_to_air",
-    ]
-    for category in metadata.frozen_data:
-        if category["include_in_homepage"]:
-            sorted_data = sorted(
-                category["metadata"], key=lambda k: k["popularity"], reverse=True
-            )
-            sorted_data = sorted_data[:data_cap_limit]
-            for item in sorted_data:
-                [item.pop(key, None) for key in unwanted_keys]
-            category["metadata"] = sorted_data
-            categories_data.append(category)
+            random.seed(100)
+            categories_data = []
+            carousel_data = []
+            top_rated_series_data = []
+            top_rated_movies_data = []
+            newly_added_movies_data = []
+            newly_added_episodes_data = []
+            unwanted_keys = [
+                "cast",
+                "seasons",
+                "file_name",
+                "subtitles",
+                "external_ids",
+                "collection",
+                "homepage",
+                "last_episode_to_air",
+                "next_episode_to_air",
+            ]
+            for category in metadata.frozen_data:
+                if category["include_in_homepage"]:
+                    sorted_data = sorted(
+                        category["metadata"], key=lambda k: k["popularity"], reverse=True
+                    )
+                    sorted_data = sorted_data[:data_cap_limit]
+                    for item in sorted_data:
+                        [item.pop(key, None) for key in unwanted_keys]
+                    category["metadata"] = sorted_data
+                    categories_data.append(category)
 
-    for data_type, data in deepcopy(metadata.sorted).items():
-        carousel_sorted_data = sorted(
-            deepcopy(data), key=lambda k: k["popularity"], reverse=True
-        )
-        carousel_sorted_data = carousel_sorted_data[:3]
-        for item in carousel_sorted_data:
-            [item.pop(key, None) for key in unwanted_keys]
-        carousel_data.extend(carousel_sorted_data)
+            for data_type, data in deepcopy(metadata.sorted).items():
+                carousel_sorted_data = sorted(
+                    deepcopy(data), key=lambda k: k["popularity"], reverse=True
+                )
+                carousel_sorted_data = carousel_sorted_data[:3]
+                for item in carousel_sorted_data:
+                    [item.pop(key, None) for key in unwanted_keys]
+                carousel_data.extend(carousel_sorted_data)
 
-        top_rated_sort_data = sorted(
-            deepcopy(data), key=lambda k: k["rating"], reverse=True
-        )
-        top_rated_sort_data = top_rated_sort_data[:data_cap_limit]
-        for item in top_rated_sort_data:
-            [item.pop(key, None) for key in unwanted_keys]
-        if data_type == "movies":
-            top_rated_movies_data.extend(top_rated_sort_data)
-            new_movies_sort_data = sorted(
-                deepcopy(data), key=lambda k: k["modified_time"], reverse=True
-            )
-            new_movies_sort_data = new_movies_sort_data[:data_cap_limit]
-            for item in new_movies_sort_data:
-                [item.pop(key, None) for key in unwanted_keys]
-            newly_added_movies_data.extend(new_movies_sort_data)
-        elif data_type == "series":
-            top_rated_series_data.extend(top_rated_sort_data)
-            all_episodes = []
-            for item in data:
-                for season in item["seasons"]:
-                    for episode in season["episodes"]:
-                        episode.update(
-                            {
-                                "season_number": season["season_number"],
-                                "series_id": item["id"],
-                                "series_name": item["title"],
-                                "series_poster": item["poster_url"],
-                                "season_name": season["name"],
-                                "season_poster": season["poster_path"],
-                                "tmdb_id": item["tmdb_id"],
-                            }
-                        )
-                        all_episodes.append(episode)
-            newly_added_episodes_sort_data = sorted(
-                deepcopy(all_episodes),
-                key=lambda k: k.get("air_date") or "",
-                reverse=True,
-            )
-            newly_added_episodes_data = newly_added_episodes_sort_data[:data_cap_limit]
-    random.shuffle(carousel_data)
+                top_rated_sort_data = sorted(
+                    deepcopy(data), key=lambda k: k["rating"], reverse=True
+                )
+                top_rated_sort_data = top_rated_sort_data[:data_cap_limit]
+                for item in top_rated_sort_data:
+                    [item.pop(key, None) for key in unwanted_keys]
+                if data_type == "movies":
+                    top_rated_movies_data.extend(top_rated_sort_data)
+                    new_movies_sort_data = sorted(
+                        deepcopy(data), key=lambda k: k["modified_time"], reverse=True
+                    )
+                    new_movies_sort_data = new_movies_sort_data[:data_cap_limit]
+                    for item in new_movies_sort_data:
+                        [item.pop(key, None) for key in unwanted_keys]
+                    newly_added_movies_data.extend(new_movies_sort_data)
+                elif data_type == "series":
+                    top_rated_series_data.extend(top_rated_sort_data)
+                    all_episodes = []
+                    for item in data:
+                        for season in item["seasons"]:
+                            for episode in season["episodes"]:
+                                episode.update(
+                                    {
+                                        "season_number": season["season_number"],
+                                        "series_id": item["id"],
+                                        "series_name": item["title"],
+                                        "series_poster": item["poster_url"],
+                                        "season_name": season["name"],
+                                        "season_poster": season["poster_path"],
+                                        "tmdb_id": item["tmdb_id"],
+                                    }
+                                )
+                                all_episodes.append(episode)
+                    newly_added_episodes_sort_data = sorted(
+                        deepcopy(all_episodes),
+                        key=lambda k: k.get("air_date") or "",
+                        reverse=True,
+                    )
+                    newly_added_episodes_data = newly_added_episodes_sort_data[:data_cap_limit]
+            random.shuffle(carousel_data)
+            
+            cache.set("categories_data", categories_data)
+            cache.set("carousel_data", carousel_data)
+            cache.set("top_rated_series_data", top_rated_series_data)
+            cache.set("top_rated_movies_data", top_rated_movies_data)
+            cache.set("newly_added_movies_data", newly_added_movies_data)
+            cache.set("newly_added_episodes_data", newly_added_episodes_data)
+        else:
+            categories_data = cache.get("categories_data")
+            carousel_data = cache.get("carousel_data")
+            top_rated_movies_data = cache.get("top_rated_movies_data")
+            top_rated_series_data = cache.get("top_rated_series_data")
+            newly_added_episodes_data = cache.get("newly_added_episodes_data")
+            newly_added_movies_data = cache.get("newly_added_movies_data")
     end = time.perf_counter()
     return {
         "ok": True,
