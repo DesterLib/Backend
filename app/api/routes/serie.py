74ba4da63d4878041903f7deb20d194dbe15b810
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-06-09 17:11:16 +0400

    NEW: series route

--- app/api/routes/serie.py
+++ app/api/routes/serie.py
@@ -4,34 +4,28 @@ from time import perf_counter
 
 
 router = APIRouter(
-    prefix="/movie",
+    prefix="/serie",
     tags=["internals"],
 )
 
 
 @router.get("/{id}", response_model=Dict[str, Any], status_code=200)
-def movie(id: int) -> Dict[str, Any]:
+def serie(id: int) -> Dict[str, Any]:
     start = perf_counter()
     from main import mongo
 
-    results = {}
-    for category in mongo.config["categories"]:
-        if category["type"] == "movies":
-            result = mongo.movies_col.find({"tmdb_id": id}, {"_id": 0})
-            for item in result:
-                if results == {}:
-                    results = item
-                    results["id"] = [results["id"]]
-                    results["file_name"] = [results["file_name"]]
-                    results["modified_time"] = [results["modified_time"]]
-                else:
-                    results["id"].append(item["id"])
-                    results["file_name"].append(item["file_name"])
-                    results["modified_time"].append(item["modified_time"])
-
-    return {
-        "ok": True,
-        "message": "success",
-        "results": results,
-        "time_taken": perf_counter() - start,
-    }
+    results = list(mongo.series_col.find({"tmdb_id": id}, {"_id": 0}))
+    if len(results) > 0:
+        result = results[0]
+        return {
+            "ok": True,
+            "message": "success",
+            "results": result,
+            "time_taken": perf_counter() - start,
+        }
+    else:
+        return {
+            "ok": False,
+            "message": "No matches found.",
+            "time_taken": perf_counter() - start,
+        }
