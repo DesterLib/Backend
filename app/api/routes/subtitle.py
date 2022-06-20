3cc3ca9a6de486c51b6799dac11ffa6733fd5ed7
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-06-20 08:31:20 +0000

    ENHANCE: better subtitles

--- app/api/routes/subtitle.py
+++ app/api/routes/subtitle.py
@@ -1,7 +1,10 @@
+from typing import Union, Optional
+from xmlrpc.client import boolean
 import requests
 from fastapi import APIRouter
 from time import perf_counter
 from app.models import DResponse
+from fastapi.responses import RedirectResponse
 
 
 router = APIRouter(
@@ -11,21 +14,22 @@ router = APIRouter(
 
 
 @router.get("/{id}", status_code=200)
-def subtitle(id: int) -> dict:
+def subtitle(id: int, permanent: Optional[boolean] = True) -> Union[dict, RedirectResponse]:
     init_time = perf_counter()
     from main import mongo
 
     os_api_key = mongo.config["subtitles"].get("api_key")
-    result = requests.post(
-        "https://api.opensubtitles.com/api/v1/download",
-        headers={"Api-Key": os_api_key},
-        data={"file_id": id},
-    ).json()
-
-    return DResponse(
-        200,
-        f"Successfully retrieved subtitle information.",
-        True,
-        result,
-        init_time,
-    ).__json__()
+    if os_api_key:
+        result = requests.post(
+            "https://api.opensubtitles.com/api/v1/download",
+            headers={"Api-Key": os_api_key},
+            data={"file_id": id},
+        ).json()["link"]
+        if permanent is not True:
+            return RedirectResponse(result, status_code=307)
+        else:
+            return RedirectResponse(result, status_code=308)
+    else:
+        return DResponse(
+            401, "No Open Subtitles API key was provided.", False, None, init_time
+        ).__json__()
